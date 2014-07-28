a3b7c7c319af17b54826258d67ac5614c3ba99cd
sreecodeslayer <kesav.tc8@gmail.com>
2014-07-28 21:59:05 +0530

    Register Form, more edits

--- newaccount.cpp
+++ newaccount.cpp
@@ -1,9 +1,11 @@
 #include "newaccount.h"
 #include "ui_newaccount.h"
 #include <QString>
+#include <QDebug>
 
 QString New_username;
 QString New_password;
+QString Confirm_password;
 
 
 NewAccount::NewAccount(QWidget *parent) :
@@ -20,5 +22,16 @@ NewAccount::~NewAccount()
 
 void NewAccount::on_Register_Button_clicked()
 {
+    New_username = ui->Username_Reg->text();
+    New_password = ui->Password_Reg->text();
+    Confirm_password = ui->ConfirmPass_Reg->text();
 
+    if(New_password == Confirm_password)
+    {
+        //Create Account
+    }
+    else
+    {
+        //Re-Confirm
+    }
 }
